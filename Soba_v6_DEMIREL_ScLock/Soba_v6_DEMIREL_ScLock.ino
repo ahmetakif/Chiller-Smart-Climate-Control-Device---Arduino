922479881fb1fd22461c6a2bbd7178e1ef75ba7a
Ahmet Akif Kaya <ahmetakifkaya11@gmail.com>
2020-03-13 00:22:02 +0300

    Update Soba_v6_DEMIREL_ScLock.ino

--- Soba_v6_DEMIREL_ScLock/Soba_v6_DEMIREL_ScLock.ino
+++ Soba_v6_DEMIREL_ScLock/Soba_v6_DEMIREL_ScLock.ino
@@ -1,11 +1,28 @@
 ///////////////////////////////////////////////////
-// This project is developed by @ahmetakifkaya
-//for Uzerman Muhendislik to be used as a 
-//controller for the chiller devices in 
-//Suleymen Demirel Kultur Merkezi in KBB
-//
-//
-//
+//######## CHILLER-SMART-CLIMATE-CONTROL-DEVICE #########
+//#
+//# Author: Ahmet Akif Kaya
+//# Date: 08/25/19
+//# Description:
+//# This project is developed by @ahmetakifkaya
+//# for Uzerman Muhendislik to be used as a 
+//# controller for the chiller devices in 
+//# Suleymen Demirel Culture Centre in KBB
+
+//# This code is specifically written for the Chiller-Smart-Climate-Control-Device project by Ahmet Akif Kaya
+//# and all rights of this program is owned by Ahmet Akif Kaya under APACHE open source license. 
+//# You are automatically accepting the terms of the APACHE license by using this code in any way.
+//# The terms and conditions about this project's APACHE license can be found in the file named
+//# "LICENSE" in the root directory of this repository.
+
+//# Copyright 2020 Ahmet Akif Kaya
+//# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. 
+//# You may obtain a copy of the License at
+//# http://www.apache.org/licenses/LICENSE-2.0
+//# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+//# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+//# language governing permissions and limitations under the License.
+// 
 //    ///////////////    ////////////////
 //    //           //    //            //
 //    //  ///////////    ////////  //////
@@ -21,11 +38,10 @@
 //   ////////////////////////////////////////
 //   ////////////////////////////////////////
 //
-//
-//
 ////////////////////////////////////////////////////
 // https://github.com/ahmetakif
-// https://www.uzerman.com/
+// http://ahmetakifkaya.com
+// https://www.uzerman.com
 ////////////////////////////////////////////////////
 
 #include <Nextion.h>
